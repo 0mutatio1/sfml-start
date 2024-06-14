160c460edbc7898bcb62ac750d91e834726fd85d
stone <942225458@qq.com>
2024-06-14 17:51:27 +0800

    resource manage

---
+++ src/ch01_02/StringHelpers.h
@@ -0,0 +1,16 @@
+//
+// Created by stone on 2024-06-13.
+//
+
+#ifndef STRINGHELPERS_H
+#define STRINGHELPERS_H
+
+#include <sstream>
+
+
+template <typename T>
+std::string toString(const T& value);
+
+#include "StringHelpers.inl"
+
+#endif //STRINGHELPERS_H
