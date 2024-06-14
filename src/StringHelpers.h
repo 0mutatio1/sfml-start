61134bc363465df4c28703888233e1badbe5c22b
stone <942225458@qq.com>
2024-06-14 18:39:45 +0800

    template method impl

--- src/StringHelpers.h
+++ src/StringHelpers.h
@@ -11,6 +11,12 @@
 template <typename T>
 std::string toString(const T& value);
 
-#include "StringHelpers.inl"
+template <typename T>
+std::string toString(const T& value)
+{
+    std::stringstream stream;
+    stream << value;
+    return stream.str();
+}
 
 #endif //STRINGHELPERS_H
